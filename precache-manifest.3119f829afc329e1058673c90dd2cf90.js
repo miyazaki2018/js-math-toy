d8bb9e864560cf01c9bcd1cd7da90d40bce115a0
jubaopen <jubaopen@wacai.com>
2019-05-01 23:46:13 +0800

    Updates

---
+++ precache-manifest.3119f829afc329e1058673c90dd2cf90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2ba71461e0f46c360e35c162d1c01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad2f96e26b7f791078e4",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ad2f96e26b7f791078e4",
+    "url": "/static/js/main.4f2a7dad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
